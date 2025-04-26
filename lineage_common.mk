8e55edb15f9218f212a0d9369e328c3b93d01ade
ppanzenboeck <panz.development@gmail.com>
2025-04-26 17:08:24 +0200

    gs201: remove packages [blueprint]

--- lineage_common.mk
+++ lineage_common.mk
@@ -42,3 +42,7 @@ $(call inherit-product-if-exists, vendor/google/camera/config.mk)
 
 # ViPER4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# Remove Packages
+PRODUCT_PACKAGES += \
+  RemovePackages
