1dad43b773d93719d99c2e62dad93e3299576f8b
Yaznas <sanjshrestha53@gmail.com>
2025-04-29 21:39:45 +0545

    gs201: Add V4A

--- lineage_common.mk
+++ lineage_common.mk
@@ -41,4 +41,7 @@ include hardware/google/pixel/touch/device.mk
 # Pixel Camera
 $(call inherit-product-if-exists, vendor/google/camera/config.mk)
 
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 DERPFEST_BUILD_TYPE := Official
